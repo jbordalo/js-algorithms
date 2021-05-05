6503580e1464bf68aff1886a895cbae1ff5fff36
jbordalo <joao-bordalo@hotmail.com>
2021-05-05 11:52:53 +0100

    Add miscellaneous algorithms
    Algorithm to compare two arrays.
    Algorithm to check if a word is an anagram of another.
    Algorithm to count the number of unique values in an array.

---
+++ misc/anagram.js
@@ -0,0 +1,22 @@
+function validAnagram(first, second) {
+    if (first.length != second.length) return false;
+
+    let lookup = {};
+
+    for (let i = 0; i < first.length; i++) {
+        let letter = first[i];
+
+        lookup[letter] ? lookup[letter]++ : lookup[letter] = 1;
+
+    }
+
+    for (let i = 0; i < second.length; i++) {
+        let letter = second[i]
+        if (!lookup[letter]) return false;
+        else {
+            lookup[letter]--;
+        }
+    }
+    return true;
+}
+
---
+++ misc/countUniqueValues.js
@@ -0,0 +1,14 @@
+function countUniqueValues(arr) {
+    if (arr.length === 0) return 0;
+    let i = 0;
+    // let j = 1;
+
+    for (let j = 1; j < arr.length; j++) {
+        if (arr[i] !== arr[j]) {
+            i++;
+            arr[i] = arr[j];
+        }
+    }
+    return i+1;
+}
+
